89bfbb2815e16f61490caa7e75e7c9191c0ad426
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-15 23:32:43 +0100

    pagination component done

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppAlert from './components/AppAlert.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
+import AppPagination from './components/AppPagination.vue';
 // Server PHP Laravel con /api !
 const apiBaseUrl = 'http://127.0.0.1:8000/api'
 export default {
@@ -10,21 +11,25 @@ export default {
   components: {
     AppHeader,
     ProjectsList,
-    AppAlert
+    AppAlert,
+    AppPagination
   },
   data: () => ({
     isLoading: false,
     isALertOpen: false,
+    // Mettere tutto sotto la stessa chiave cosi con il Destructuring(sotto) prenderò gli elementi parlanti es : project.links
     projects: {
       data: [],
       links: []
     },
   }),
   methods: {
-    fetchProjects() {
+    fetchProjects(endpoint = null) {
       // Loading alla chiamata a true(on)
       this.isLoading = true;
-      axios.get(apiBaseUrl + '/projects').then(res => {
+      // Se l'endpoint non me lo dai sarà basico altrimenti se me lo passi andrà dove gli diremo noi ( link.url che sara la pagina succ o previous)
+      if (!endpoint) endpoint = apiBaseUrl + '/projects';
+      axios.get(endpoint).then(res => {
         // Destructuring : tiro fuori data e links da res.data e lo riassegno in project dando solo data e links che sono vuoti inizialmente
         const { data, links } = res.data;
         this.projects = { data, links };
@@ -57,13 +62,6 @@ export default {
     <app-loader v-if="isLoading"></app-loader>
     <!-- ALtrimenti v-else vai vedere la projectsList -->
     <projects-list v-else :projects="projects.data"></projects-list>
+    <app-pagination :links="projects.links" @change-page="fetchProjects"></app-pagination>
   </main>
-
-  <!-- <footer>
-      <ul class="pagination">
-        <li v-for="link in project.links" :key="link.label" class="page-item" :class="{ 'active': link.active }">
-          <a class="page-item" href="">1</a>
-        </li>
-      </ul>
-    </footer> -->
 </template>
---
+++ src/components/AppPagination.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'AppPagination',
+    props: { links: Array },
+}
+</script>
+
+<template>
+    <nav>
+        <ul class="pagination">
+            <!-- Classi : sarà active (true o false) prendendo dalla chiamata link.active e disabled quando !non avrà link url
+                    Lo stesso per i bottoni (bottoni perche con le a ricarica la pagina e :disabled stessa logica ) 
+                    v-html per interpretare il linguaggio html passato per i bottoni next e previous-->
+            <li v-for="link in links" :key="link.label" class="page-item"
+                :class="[{ 'active': link.active }, { 'disabled': !link.url }]">
+
+                <button @click="$emit('change-page', link.url)" class="page-link" href="#" :disabled="!link.url"
+                    v-html="link.label"></button>
+            </li>
+        </ul>
+    </nav>
+</template>
