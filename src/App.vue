98267e19d0b8603d2dcae5d37a686f8766ab15de
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-15 18:27:05 +0100

    rendering simple list of Projects

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import ProjectsList from './components/projects/ProjectsList.vue';
+// Server PHP Laravel con /api !
+const apiBaseUrl = 'http://127.0.0.1:8000/api'
 export default {
   name: 'App',
   components: {
-    AppHeader
+    AppHeader,
+    ProjectsList,
+  },
+  data: () => ({
+    projects: []
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(apiBaseUrl + '/projects').then(res => {
+        // In res.data arrivano i dati della chiamata
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
   }
 }
 </script>
 
 <template>
   <app-header></app-header>
+  <main class="container">
+    <projects-list :projects="projects"></projects-list>
+  </main>
 </template>
---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'ProjectsList',
+    props: { projects: Array }
+};
+</script>
+
+<template>
+    <section id="projects-list" class="my-4">
+        <ul v-if="projects.length">
+            <li>
+            <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+            </li>
+        </ul>
+        <h3 v-else class="text-center">No Projects available</h3>
+    </section>
+</template>
