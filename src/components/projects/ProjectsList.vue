b6e4e92053b2ec344181119d1d21ccb77b882b48
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-15 20:58:43 +0100

    projects list

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,17 +1,19 @@
 <script>
+import ProjectCard from './ProjectCard.vue';
 export default {
     name: 'ProjectsList',
+    components: { ProjectCard },
     props: { projects: Array }
 };
 </script>
 
 <template>
     <section id="projects-list" class="my-4">
-        <ul v-if="projects.length">
-            <li>
-            <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
-            </li>
-        </ul>
+        <div v-if="projects.length">
+            <project-card v-for="project in projects" :key="project.id" :project="project">
+                {{ project.title }}
+            </project-card>
+        </div>
         <h3 v-else class="text-center">No Projects available</h3>
     </section>
 </template>
