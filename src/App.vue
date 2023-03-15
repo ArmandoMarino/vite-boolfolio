b1b1c82eb6998606e1926139d32ec37e302d0788
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-15 17:25:57 +0100

    app header created and imported base

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <app-header></app-header>
+</template>
