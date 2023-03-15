3a6d9a691a35bdc4da8a1d280ca53ed53d96ae2d
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-15 22:04:34 +0100

    imported loader on mainjs

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
-createApp(App).mount('#app')
+import AppLoader from './components/AppLoader.vue';
+// Default App create e mount insieme
+// createApp(App).mount('#app')
+
+// Spezzo per mettere "qualcosa" di mezzo
+const app = createApp(App);
+
+app.component('AppLoader', AppLoader);
+
+app.mount('#app');
+
+
+
