cc8c3464b2dc977f8086814785ef46cd40efe934
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-15 22:57:43 +0100

    alert.vue

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppAlert',
+    // Props passate per logica chiudi apri
+    props: { isOpen: Boolean },
+    emits: ['close']
+}
+</script>
+
+<template>
+    <!-- V-if se aperto e onclick passa close-->
+    <div v-if="isOpen" class="alert alert-warning alert-dismissible fade show w-50 m-auto my-3" role="alert">
+        <strong>Error</strong> An Error has occurred.
+        <button type="button" @click="$emit('close')" class="btn-close"></button>
+    </div>
+</template>
+
