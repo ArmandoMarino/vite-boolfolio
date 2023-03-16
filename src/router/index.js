6e07ec87ac16708902ce1abbfa56e0f6731dce35
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-16 18:21:30 +0100

    router update

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import { createRouter, createWebHistory } from "vue-router";
 // Importo le pagine che voglio usare
 import HomePage from '../pages/HomePage.vue';
 import ContactPage from '../pages/ContactPage.vue';
+import ProjectDetailPage from '../pages/projects/ProjectDetailPage.vue';
+
 import NotFoundPage from '../pages/NotFoundPage.vue';
 
 // Creo il Router e definisco le rotte
@@ -12,10 +14,15 @@ const router = createRouter({
     history: createWebHistory(),
     // Importo la classe da avere in active
     linkExactActiveClass: 'active',
+    linkActiveClass: 'active',
+
     // Rotte che useremo
     routes: [
         { path: '/', name: 'home', component: HomePage },
         { path: '/contact', name: 'contact', component: ContactPage },
+        //Rotta per lo show del Project
+        { path: '/projects/:id', name: 'project-detail', component: ProjectDetailPage },
+
         // La funzione pathMatch con la * prende qualsiasi cosa diversa e redirect alla 404 page
         { path: '/:pathMatch(.*)*', name: 'NotFoundPage', component: NotFoundPage },
 
